597a691a64705adfbd4d46ea92d693ea6ae71658
TIHeitorDS <heitorclaudinods@gmail.com>
2023-02-20 15:16:58 -0300

    explicação da questão 1 e 2 disponível

--- alocação dinâmica vetores/lista I/q1.c
+++ alocação dinâmica vetores/lista I/q1.c
@@ -1,14 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void feedback(int quantity, int *gender, int *view) {
+void feedback(int quantity, int *gender, int *view)
+{
     int countF = 0, countM = 0;
     int xx = 0, xy = 0;
 
-    for(int i = 0; i < quantity; i++) {
-        if (gender[i] == 2 && view[i] == 1) {
+    for (int i = 0; i < quantity; i++)
+    {
+        if (gender[i] == 2 && view[i] == 1)
+        {
             countF++;
-        } else if(gender[i] == 1 && view[i] == 2) {
+        }
+        else if (gender[i] == 1 && view[i] == 2)
+        {
             countM++;
         }
     }
@@ -19,24 +24,25 @@ void feedback(int quantity, int *gender, int *view) {
     printf("porcentagem de pessoas do sexo feminino que gostaram: %d%%.", xy);
 }
 
-int main(void) {
+int main(void)
+{
     int *genderArr, *viewArr;
-    genderArr = (int*)malloc(5 * sizeof(int));
-    viewArr = (int*)malloc(5 * sizeof(int));
+    genderArr = (int *)malloc(5 * sizeof(int));
+    viewArr = (int *)malloc(5 * sizeof(int));
 
-
-    for (int i = 0; i < 5; i++){
+    for (int i = 0; i < 5; i++)
+    {
         printf("Qual o genero do %d entrevistado? \n1. M \n2. F\ngenero: ", i + 1);
         scanf("%d", &genderArr[i]);
 
         printf("\nVoce gostou do novo produto? \n1 - sim\n2 - nao\nresponta: ");
         scanf("%d", &viewArr[i]);
-    }    
+    }
 
     feedback(5, genderArr, viewArr);
 
     free(genderArr);
     free(viewArr);
-    
+
     return 0;
 }
--- alocação dinâmica vetores/lista I/q2.c
+++ alocação dinâmica vetores/lista I/q2.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main() {
+int main()
+{
     int quantityOfStudents = 10, quantityOfQuestions, aux = 0, aux2, count, q, approved = 0;
     float result;
     char *correctAnswer, *answerOfStudens;
@@ -11,22 +12,26 @@ int main() {
 
     aux2 = quantityOfQuestions;
 
-    correctAnswer = (char*) malloc(quantityOfQuestions * sizeof(char));
-    answerOfStudens = (char*) malloc(quantityOfQuestions * quantityOfStudents * sizeof(char));
+    correctAnswer = (char *)malloc(quantityOfQuestions * sizeof(char));
+    answerOfStudens = (char *)malloc(quantityOfQuestions * quantityOfStudents * sizeof(char));
 
-    if (correctAnswer == NULL || answerOfStudens == NULL) {
+    if (correctAnswer == NULL || answerOfStudens == NULL)
+    {
         printf("Memoria insuficiente!");
         exit(1);
     }
 
-    for (int i = 0; i < quantityOfQuestions; i++) {
+    for (int i = 0; i < quantityOfQuestions; i++)
+    {
         printf("Informe a resposta correta da %d questao: ", i + 1);
         scanf("%s", (correctAnswer + i));
     }
 
-    for (int j = 0; j < quantityOfStudents; j++) {
+    for (int j = 0; j < quantityOfStudents; j++)
+    {
         printf("Informe as alternativas marcadas pelo %d estudante:\n", j + 1);
-        for (int k = aux; k < aux2; k++) {
+        for (int k = aux; k < aux2; k++)
+        {
             scanf("%s", (answerOfStudens + k));
         }
 
@@ -37,13 +42,16 @@ int main() {
     aux = 0;
     aux2 = quantityOfQuestions;
     printf("\n");
-    for (int l = 0; l < quantityOfStudents; l++) {
+    for (int l = 0; l < quantityOfStudents; l++)
+    {
         result = 0;
         count = 0;
         q = 0;
 
-        for (int m = aux; m < aux2; m++) {
-            if (*(answerOfStudens + m) == *(correctAnswer + q)) {
+        for (int m = aux; m < aux2; m++)
+        {
+            if (*(answerOfStudens + m) == *(correctAnswer + q))
+            {
                 count++;
             }
 
@@ -53,25 +61,32 @@ int main() {
         aux = aux2;
         aux2 += quantityOfQuestions;
 
-        if (10 % quantityOfQuestions == 0) {
+        if (10 % quantityOfQuestions == 0)
+        {
             result = (10 / quantityOfQuestions) * count;
-        } else {
-            if (count % quantityOfQuestions != 0) {
+        }
+        else
+        {
+            if (count % quantityOfQuestions != 0)
+            {
                 result = ((float)10 / quantityOfQuestions) * (count % quantityOfQuestions);
-            } else {
+            }
+            else
+            {
                 result = (count / quantityOfQuestions) * 10;
             }
-        } 
+        }
 
-        if (result >= 6) {
+        if (result >= 6)
+        {
             approved++;
         }
 
         printf("A nota do %d aluno e: %.1f\n", l + 1, result);
     }
 
-    approved = (approved * 100) / quantityOfQuestions;
-    printf("A porcentagem de alunos aprovados e: %%%d.", approved);
+    approved = (approved * 100) / quantityOfStudents;
+    printf("A porcentagem de alunos aprovados e: %d%%.", approved);
 
     free(correctAnswer);
     free(answerOfStudens);
