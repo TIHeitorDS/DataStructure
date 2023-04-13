cf9f7e620910abeb807b62b2eecd1b31e4134165
TIHeitorDS <heitorclaudinods@gmail.com>
2023-04-13 14:00:40 -0300

    tad corrigido

--- 8. Listas encadeadas/lista.h
+++ 8. Listas encadeadas/lista.h
@@ -1,6 +1,7 @@
 typedef struct listano ListaNo;
 typedef struct lista Lista;
 
+Lista *lst_cria();
 void list_insere(Lista *l, int v);
 void list_imprime(Lista *l);
 void list_retira(Lista *l, int v);
